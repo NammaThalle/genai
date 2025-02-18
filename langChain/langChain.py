27f291ed3ec52a250750c88e46ff44d1a64a97cf
NammaThalle <manerikarshravan@gmail.com>
2025-02-18 20:40:51 +0530

    added more secure way of reading credentials

--- langChain/langChain.py
+++ langChain/langChain.py
@@ -1,10 +1,18 @@
-import getpass
 import os
+import json
+import getpass
 
-if not os.environ.get["GOOGLE_API_KEY"]:
-    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
-    os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter your LangSmith API key: ")
-    os.environ["LANGSMITH_TRACING"] = "true"
+# Read credentials from credentials.json
+def read_credentials():
+    with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'credentials.json'), 'r') as f:
+        credentials = json.load(f)
+    return credentials['credentials']
+
+credentials = read_credentials()
+
+os.environ["GOOGLE_API_KEY"] = credentials['GOOGLE_API_KEY']
+os.environ["LANGSMITH_API_KEY"] = credentials['LANGSMITH_API_KEY']
+os.environ["LANGSMITH_TRACING"] = "true"
 
 # Load the model
 def load_model():
@@ -16,7 +24,8 @@ def load_model():
 def say_hello():
     model = load_model()
     ai_message = model.invoke('Hello')
-    print(ai_message.content)
+    
+    return ai_message.content
 
 # Translate the text
 def translate_text():
@@ -26,7 +35,7 @@ def translate_text():
     promptTemplate = ChatPromptTemplate.from_messages(
         [("system", systemTemplate), ("user", "{text}")]
     )
-    prompt = promptTemplate.invoke({"language": "Hindi with English script", "text": ai_message.content})
+    prompt = promptTemplate.invoke({"language": "Hindi with English script", "text": say_hello()})
     ai_message = model.invoke(prompt)
     print(ai_message.content)
 
@@ -47,7 +56,6 @@ def load_document():
 # Split document
 def split_document(docs):
     from langchain_text_splitters import RecursiveCharacterTextSplitter
-    
 
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = 1000,
@@ -61,4 +69,5 @@ def split_document(docs):
     # print(split_docs[0].page_content)
     return split_docs
 
-split_document(docs=load_document())
+# split_document(docs=load_document())
+translate_text()
