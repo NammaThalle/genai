ea13703a634c66dcb3db029677c32fe487ba0f68
Shravan Manerikar <shravan.manerikar@konverge.ai>
2025-02-18 20:31:29 +0530

    load chat model

--- langChain/langChain.py
+++ langChain/langChain.py
@@ -5,3 +5,9 @@ if not os.environ.get["GOOGLE_API_KEY"]:
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
     os.environ["LANGSMITH_API_KEY"] = getpass.getpass("Enter your LangSmith API key: ")
     os.environ["LANGSMITH_TRACING"] = "true"
+
+# Load the model
+def load_model():
+    from langchain_google_genai import ChatGoogleGenerativeAI
+    model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+    return model
